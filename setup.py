8175d46bd8cf38cb5f37ca535afd4b55b5b23cd4
kiorky <kiorky@cryptelium.net>
2011-05-23 13:36:48 +0000

    import original code
    git-svn-id: https://subversion.makina-corpus.net/scrumpy/mars/eggs/marsapp.migration/trunk@2514 3da67ed8-b444-4088-a917-8ba57470bef7

--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
-#-*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+import os
 
-version = '0.1'
-
-
-setup(name='marsapp.policy',
+setup(name='marsapp.migration',
       version=version,
-      description="Policy package for setting up a MARS portal",
-      long_description="""\
-      """,
+      description="Package for migrating old mars items to the new system",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-          "Framework :: Plone",
+        "Framework :: Plone",
+        "Programming Language :: Python",
           "Framework :: Zope2",
-          "Framework :: Zope3",
-          "Programming Language :: Python",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
-      keywords='zope plone archaeology',
+          "Framework :: Zope3", 
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone mars migration zope  archaeology',
       author='David Convent',
-      author_email='mars@naturalsciences.be',
-      url='http://www.naturalsciences.be/metamars',
+      author_email='david.convent@naturalsciences.be',
+      url='http://svn.naturalsciences.be/projects/mars',
       license='GPL',
       namespace_packages=[                'mars', 'mars.testing', 'marsapp'],  
       packages=find_packages('src'),
