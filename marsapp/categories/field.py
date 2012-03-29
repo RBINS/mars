9f76efa24c82a1d9aa5139da91d2150c94586280
kiorky <kiorky@cryptelium.net>
2012-03-29 21:56:33 +0000

    perf improv
    git-svn-id: https://subversion.makina-corpus.net/scrumpy/mars/eggs/marsapp.categories/trunk@2895 3da67ed8-b444-4088-a917-8ba57470bef7

--- marsapp/categories/field.py
+++ marsapp/categories/field.py
@@ -3,11 +3,13 @@ from zope.component import getUtility
 
 from AccessControl import ClassSecurityInfo
 from Products.CMFCore.utils import getToolByName
+from Products.Archetypes import PloneMessageFactory as _
 #from Products.CMFCore import permissions
 
 #from Products.Archetypes.Field import Image
+from Products.Archetypes import config
 from Products.Archetypes.Field import ReferenceField
-#from Products.Archetypes.public import DisplayList
+from Products.Archetypes.public import DisplayList
 from Products.Archetypes.Registry import registerField
 from Products.Archetypes.Registry import registerPropertyType
 #from Products.Archetypes.config import TOOL_NAME
@@ -41,15 +43,101 @@ class MarscatField(ReferenceField):
         'default': None,
         'widget': MarscatWidget,
         'allowed_types': ('Mars Category',),
-        'allowed_type_column' : 'portal_type',
-        'addable': 1,
-        'destination': None,
-        'relationship': None,
-        'categories': (),
-        'index_method' : '_at_edit_accessor',
+        #'allowed_type_column' : 'portal_type',
+        #'addable': 1,
+        #'destination': None,
+        #'relationship': None,
+        #'categories': (),
+        #'index_method' : '_at_edit_accessor',
         })
 
     security  = ClassSecurityInfo()
+    def _Vocabulary(self, content_instance):
+        """overload 
+            Archetypes Products.Archetypes.Field.ReferenceField._Vocabulary to make a restriction on the _path_ for performance gain
+            """
+        pairs = []
+        pc = getToolByName(content_instance, 'portal_catalog')
+        uc = getToolByName(content_instance, config.UID_CATALOG)
+        purl = getToolByName(content_instance, 'portal_url')
+
+        allowed_types = self.allowed_types
+        allowed_types_method = getattr(self, 'allowed_types_method', None)
+        if allowed_types_method:
+            meth = getattr(content_instance,allowed_types_method)
+            allowed_types = meth(self)
+
+        skw = allowed_types and {'portal_type':allowed_types} or {}
+        brains = uc.searchResults(skw)
+
+        if self.vocabulary_custom_label is not None:
+            label = lambda b:eval(self.vocabulary_custom_label, {'b': b})
+        elif self.vocabulary_display_path_bound != -1 and len(brains) > self.vocabulary_display_path_bound:
+            at = _(u'label_at', default=u'at')
+            label = lambda b:u'%s %s %s' % (self._brains_title_or_id(b, content_instance),
+                                             at, b.getPath())
+        else:
+            label = lambda b:self._brains_title_or_id(b, content_instance)
+
+        # The UID catalog is the correct catalog to pull this
+        # information from, however the workflow and perms are not accounted
+        # for there. We thus check each object in the portal catalog
+        # to ensure it validity for this user.
+        portal_base = purl.getPortalPath()
+        path_offset = len(portal_base) + 1
+
+        abs_paths = {}
+        abs_path = lambda b, p=portal_base: '%s/%s' % (p, b.getPath())
+
+        # modifications
+        sd =  (portal_base + 
+               "/" + 
+               self.widget.getStartupDirectory(
+                   content_instance, self)).replace('//', '/')
+
+        for b in brains:
+            apath = abs_path(b)
+            if apath.startswith(sd):
+                abs_paths.update({apath:b}) 
+        # end modifications
+
+        pc_brains = pc(path=abs_paths.keys(), **skw)
+
+        for b in pc_brains:
+            b_path = b.getPath()
+            # translate abs path to rel path since uid_cat stores
+            # paths relative now
+            path = b_path[path_offset:]
+            # The reference field will not expose Refrerences by
+            # default, this is a complex use-case and makes things too hard to
+            # understand for normal users. Because of reference class
+            # we don't know portal type but we can look for the annotation key in
+            # the path
+            if self.referenceReferences is False and \
+               path.find(config.REFERENCE_ANNOTATION) != -1:
+                continue
+
+            # now check if the results from the pc is the same as in uc.
+            # so we verify that b is a result that was also returned by uc,
+            # hence the check in abs_paths.
+            if abs_paths.has_key(b_path):
+                uid = abs_paths[b_path].UID
+                if uid is None:
+                    # the brain doesn't have an uid because the catalog has a
+                    # staled object. THAT IS BAD!
+                    raise ReferenceException("Brain for the object at %s "\
+                        "doesn't have an UID assigned with. Please update your"\
+                        " reference catalog!" % b_path)
+                pairs.append((uid, label(b)))
+
+        if not self.required and not self.multiValued:
+            no_reference = _(u'label_no_reference',
+                             default=u'<no reference>')
+            pairs.insert(0, ('', no_reference))
+
+        __traceback_info__ = (content_instance, self.getName(), pairs)
+
+        return DisplayList(pairs) 
 
     def getStartupDirectory(self, instance):
         storage = getUtility(IMarscatsSettingsStorage)
