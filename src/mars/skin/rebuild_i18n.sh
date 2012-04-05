a523c5f58d009b3de02ff905f7b5fac4c114245e
kiorky <kiorky@cryptelium.net>
2012-04-05 14:14:39 +0000

    up
    git-svn-id: https://subversion.makina-corpus.net/scrumpy/mars/eggs/mars.skin/trunk@2912 3da67ed8-b444-4088-a917-8ba57470bef7

--- src/mars/skin/rebuild_i18n.sh
+++ src/mars/skin/rebuild_i18n.sh
@@ -6,7 +6,7 @@ CWD=$(dirname $0)
 cd ${CWD}
 echo "Using ${i18ndude} in ${CWD}"
 # Synchronise the .pot with the templates.
-${i18ndude} rebuild-pot --pot locales/${PRODUCTNAME}.pot --merge locales/${PRODUCTNAME}-manual.pot --create ${I18NDOMAIN} .
+${i18ndude} rebuild-pot --pot locales/${PRODUCTNAME}.pot --merge locales/${PRODUCTNAME}-manual.pot --create ${I18NDOMAIN} . ../../../../mars.policy/
 # Synchronise the resulting .pot with the .po files
 for po in locales/*/LC_MESSAGES/${PRODUCTNAME}.po;do 
     ${i18ndude} sync --pot locales/${PRODUCTNAME}.pot $po
