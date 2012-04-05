ad1cb4edb9a0d36e4d62373a01e008a92eb48424
kiorky <kiorky@cryptelium.net>
2012-04-05 10:59:40 +0000

    up
    git-svn-id: https://subversion.makina-corpus.net/scrumpy/mars/eggs/marsapp.categories/trunk@2908 3da67ed8-b444-4088-a917-8ba57470bef7

--- marsapp/categories/widget.py
+++ marsapp/categories/widget.py
@@ -9,20 +9,22 @@ from Products.CMFCore.utils import getToolByName
 
 from Products.Archetypes.Registry import registerWidget
 
-from archetypes.referencebrowserwidget.widget import ReferenceBrowserWidget
+from archetypes.referencebrowserwidget.widget import ReferenceBrowserWidget as BReferenceBrowserWidget
 
-class ReferenceBrowserWidget(ReferenceBrowserWidget):
-    _properties = ReferenceBrowserWidget._properties.copy()
+class ReferenceBrowserWidget(BReferenceBrowserWidget):
+    _properties = BReferenceBrowserWidget._properties.copy()
     _properties.update({
         'macro': 'marsrefbrowser',
         'image_portal_types':['Image',],
         'image_method':'image_thumb', 
     })
 
-class MarscatWidget(ReferenceBrowserWidget):
-    _properties = ReferenceBrowserWidget._properties.copy()
+class MarscatWidget(BReferenceBrowserWidget):
+    _properties = BReferenceBrowserWidget._properties.copy()
     _properties.update({
         'macro': 'marscats4browser',
+        'image_portal_types':['Image',],
+        'image_method':'image_thumb',  
         'popup_name' : 'mars_popup',
         'restrict_browsing_to_startup_directory': True,
         })
@@ -33,4 +35,14 @@ registerWidget(MarscatWidget,
                  'search the portal for categories to refer to.'),
     used_for=('Products.marscats.Field.MarscatField',)
     )
+
+
+
+registerWidget(ReferenceBrowserWidget,
+           title='Reference Browser',
+               description=('Reference widget that allows you to browse or '
+                            'search the portal for objects to refer to.'),
+               used_for=('Products.Archetypes.Field.ReferenceField',)
+               ) 
+
 
