bebf6c661e6f3dcc5307354b1d382bec2a5e93a5
kiorky <kiorky@cryptelium.net>
2012-04-18 17:31:36 +0000

    first pass on fields correction
    git-svn-id: https://subversion.makina-corpus.net/scrumpy/mars/eggs/mars.policy/trunk@2947 3da67ed8-b444-4088-a917-8ba57470bef7

--- src/mars/policy/migrations/v19/__init__.py
+++ src/mars/policy/migrations/v19/__init__.py
@@ -14,6 +14,11 @@ except:
 from Products.CMFCore.utils import getToolByName
 
 
+from Products.Archetypes.Field import StringField, IntegerField
+
+from pprint import pprint
+import transaction
+
 
 def upgrade(portal_setup):
     """
@@ -28,5 +33,57 @@ def upgrade(portal_setup):
     #portal_setup.runImportStepFromProfile('profile-mars.policy:default', 'atcttool', run_dependencies=False)
     #portal_setup.runAllImportStepsFromProfile('profile-collective.js.datatables:default')
     #portal_setup.runAllImportStepsFromProfile('profile-eea.facetednavigation:default')
+    tt = site.portal_types
+    indexes =  catalog.indexes()
+    request = site.REQUEST
+    mars = site['collections']
+    for item in tt.objectIds():
+        fti = tt.restrictedTraverse(item)
+        if 'marsapp.content' in fti.product:
+            l.error('-'*80)
+            l.error(fti.id) 
+            iid = 'formigrationdeleteme'
+            try:
+                if iid in mars.objectIds():
+                    mars.manage_delObjects([id])
+                try:
+                    id = mars.invokeFactory(fti.content_meta_type, iid)
+                except ValueError, e:
+                    try:
+                        id = mars.invokeFactory(fti.title, iid)
+                    except ValueError, e:
+                        id = mars.invokeFactory(fti.id, iid)
+            except Exception, e:
+                import pdb;pdb.set_trace()  ## Breakpoint ##
 
+            inst = mars[id]
+            filtered = ['locallyAllowedTypes', 
+                        'immediatelyAddableTypes']
+            try:
+                if inst is not None:
+                    for key in inst.schema.keys():
+                        field = inst.schema[key]
+                        if field.__name__ in filtered:
+                            l.error('Skipping filtered: %s' % field.__name__)
+                            continue
+                        if getattr(field, 'vocabulary', None):
+                            notwanted = ['categorization', 'settings']
+                            if not field.schemata in notwanted:
+                                ik = field.accessor
+                                if (isinstance(field, StringField)
+                                    or isinstance(field, IntegerField) 
+                                   ):
+                                    if ik in indexes:
+                                        if catalog.Indexes[ik].meta_type != 'KeywordIndex':
+                                            l.error('Recreating %s' % ik)
+                                            catalog.delIndex(ik)
+                                            catalog.addIndex(ik, 'KeywordIndex')
+                                            catalog.reindexIndex(ik, request)
+                                            transaction.commit()
+                                        else:
+                                            print "Already done for %s" % ik
+            finally:
+                mars.manage_delObjects([id])
+
+                
 
