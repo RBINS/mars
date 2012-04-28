ea7e6cbe22536f0512c61b6d6a810af2efd89a8b
kiorky <kiorky@cryptelium.net>
2012-04-28 13:10:41 +0000

    up
    git-svn-id: https://subversion.makina-corpus.net/scrumpy/mars/eggs/mars.policy/trunk@2961 3da67ed8-b444-4088-a917-8ba57470bef7

---
+++ src/mars/policy/migrations/v23/__init__.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+
+from DateTime import DateTime
+import datetime
+import logging
+from Products.BTreeFolder2.BTreeFolder2 import BTreeFolder2Base as BTreeFolder
+from Acquisition import aq_base
+
+try:
+    from Products.CMFPlone.migrations import migration_util
+except:
+    #plone4
+    from plone.app.upgrade import utils as migration_util
+
+from Products.CMFCore.utils import getToolByName
+
+
+from Products.Archetypes.Field import StringField, IntegerField
+
+from pprint import pprint
+import transaction
+
+
+
+from marsapp.content.schemata.vocabularies import REFERENCE_SAMPLES, DISCOVERY_PLACES
+def upgrade(portal_setup):
+    """
+    """
+    l = logging.getLogger('mars/policy/migrations/v23')
+    site = portal_setup.aq_parent
+    catalog = site.portal_catalog
+    request = site.REQUEST
+    # install Products.PloneSurvey and dependencies
+    #migration_util.loadMigrationProfile(site, 'profile-Products.PloneSurvey:default')
+    #migration_util.loadMigrationProfile( site, 'profile-mars.policy.migrations.v23:22-23')
+    #portal_setup.runImportStepFromProfile('profile-collective.zipfiletransport:default', 'actions')
+    #portal_setup.runImportStepFromProfile('profile-mars.policy:default', 'catalog', run_dependencies=False)
+    portal_setup.runImportStepFromProfile('profile-mars.policy:default', 'jsregistry', run_dependencies=False)
+    #portal_setup.runAllImportStepsFromProfile('profile-collective.js.datatables:default')
+    #portal_setup.runAllImportStepsFromProfile('profile-plone.formwidget.datetime:default')
+    ## reindex new indexes
+    #for i in catalog.indexes():
+    #    if not i in indexes:
+    #        l.error('Reindexing %s' % i)
+    #        catalog.reindexIndex(i, request)
+
+    # migrate fields to datetimefields
+
+
+    #portal_setup.runAllImportStepsFromProfile('profile-eea.facetednavigation:default')
+                
+
