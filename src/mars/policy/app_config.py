9e93be78259f924beb860a98996c6612195284a8
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2012-09-04 19:45:12 +0200

    refresh

--- src/mars/policy/app_config.py
+++ src/mars/policy/app_config.py
@@ -16,44 +16,9 @@ PRODUCT_DEPENDENCIES = (\
 EXTENSION_PROFILES = ('mars.policy:default',)
 
 SKIN = 'mars.skin'
-HIDDEN_PRODUCTS = [u'plone.app.openid', u'NuPlone',
-#      u'plone.app.blob',
-#      u'plone.app.dexterity',
-#      u'Products.TinyMCE',
-#      u'Products.ContentWellPortlets',
-#      u'Products.csvreplicata',
-#      u'plone.app.z3cform',
-    #with_ploneproduct_dexterity
+HIDDEN_PRODUCTS = [u'plone.app.openid', u'NuPlone',]
 
-#    u'plone.app.dexterity',
-    #with_ploneproduct_contentwellportlet
-
-#    u'ContentWellPortlets',
-    #with_ploneproduct_csvreplica
-         #'csvreplicata',
-#        #with_ploneproduct_tinymce
-
-#    u'TinyMCE',
-#    u'mars.skin',
-#    u'mars.tma',
-#    u'mars.policy.migrations.v1_1',
-#    u'mars.policy.migrations',
-#    u'csvreplicata',
-#    u'Products.csvreplicata',
-]
-HIDDEN_PROFILES = [u'plone.app.openid', u'NuPlone',
-    u'mars.skin',
-    u'mars.tma',
-    u'mars.policy.migrations.v1_1',
-    u'mars.policy.migrations',
-      u'plone.app.blob',
-      u'plone.app.dexterity',
-      u'Products.TinyMCE',
-      u'Products.ContentWellPortlets',
-      u'Products.csvreplicata',
-      u'plone.app.z3cform',
-#    u'csvreplicata',
-#    u'Products.csvreplicata',
+HIDDEN_PROFILES = [
 
 ]
 
