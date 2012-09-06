819a328d5b8ad93daea9581ced990d64bec74731
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2012-09-06 19:04:52 +0200

    policy is working

--- src/mars/policy/setuphandlers.py
+++ src/mars/policy/setuphandlers.py
@@ -11,6 +11,7 @@ from marsapp.categories.storage import CAT_CONTAINER
 
 import logging
 from Products.CMFCore.utils import getToolByName
+from mars.policy.app_config import PRODUCT_DEPENDENCIES
 
 from lxml import etree
 
@@ -22,23 +23,37 @@ def setupVarious(context):
     care of by other handlers.
     """
     # Only run step if a flag file is present (e.g. not an extension profile)
-    if context.readDataFile('mars_various.txt') is None:
+    if context.readDataFile(
+        'mars.policy_various.txt') is None:
         return
     site = context.getSite()
-    site.setLayout('welcome.html')
     existing = site.objectIds()
-    wftool = getToolByName(site, 'portal_workflow')
-    if 'front-page' in existing:
-        fp = site['front-page']
-        if wftool.getInfoFor(fp, 'review_state') != 'private':
-            wftool.doActionFor(fp, 'retract')
-        # Exclude from navigation
-        fp.update(excludeFromNav=True)
-
-    return
-
-
-from mars.policy.app_config import PRODUCT_DEPENDENCIES
+    csvt = getToolByName(site, 'portal_csvreplicatatool')
+    portal = getToolByName(site, 
+                           'portal_url').getPortalObject()
+    csvt.setPlainFormat(True)
+    # activate all schemata for all objects.
+    csvt.fullactivation()
+    struct = {
+        'administration':{'t':'Administration',
+                          'nav':True,
+                          'tp':'Folder'},
+        'collections':{'t':'Mars',
+                       'nav':True,
+                       'tp':'Folder'},
+        CAT_CONTAINER: {'t':'Mars Categories',
+                        'nav':False,
+                        'tp':'Categories Container',},
+    }
+    for b in struct:
+        if not b in portal.objectIds():
+            obj = _createObjectByType(
+                struct[b]['tp'],
+                portal, b)
+            obj.processForm()
+            obj.setExcludeFromNav(not struct[b]['nav'])
+            obj.setTitle(struct[b]['t'])
+            publish_all(obj)
 
 def setupQi(context):
     """Miscellanous steps import handle.
@@ -53,7 +68,7 @@ def setupQi(context):
     if context.readDataFile('mars.policy_qi.txt') is None:
         return
 
-    portal = context.getSite() 
+    portal = context.getSite()
     portal_quickinstaller = getToolByName(portal, 'portal_quickinstaller')
     portal_setup = getToolByName(portal, 'portal_setup')
     logger = logging.getLogger('mars.policy.Install')
@@ -65,13 +80,13 @@ def setupQi(context):
             transaction.savepoint()
 
 
-def publish_all(context):            
+def publish_all(context):
     url = getToolByName(context, 'portal_url')
     site = url.getPortalObject()
     catalog = getToolByName(site, 'portal_catalog')
     wftool = getToolByName(site, 'portal_workflow')
     brains = catalog.search({
-        'path': {'query': 
+        'path': {'query':
                  '/'.join(context.getPhysicalPath())},
         'review_state': 'private',
     })
@@ -84,42 +99,101 @@ def createCategories(context):
     Create needed categories container and contained content.
     """
     # Only run step if a flag file is present
-    filenames = context.readDataFile('mars_create_categories.txt')
+    filenames = context.readDataFile('mars_categories_container.txt')
     if filenames is None:
         return
-    filenames = [f 
-                 for f in filenames.split('\n') 
-                 if f and not f.startswith('#') ]
+    #filenames = [f
+    #             for f in filenames.split('\n')
+    #             if f and not f.startswith('#') ]
     site = context.getSite()
     if not CAT_CONTAINER in site.objectIds():
-        _createObjectByType('Categories Container', 
+        _createObjectByType('Categories Container',
                             site,
                             id=CAT_CONTAINER,
-                            title='Mars Categories', 
+                            title='Mars Categories',
                             excludeFromNav=True)
     container = getattr(site, CAT_CONTAINER)
     publish_all(container)
-    for filename in filenames:
-        datafile = context.readDataFile('categories/'+filename)
-        container.importCatsFromText(datafile, filename)
+    #for filename in filenames:
+    #    datafile = context.readDataFile('categories/'+filename)
+    #    container.importCatsFromText(datafile, filename)
 
 def remove_pacollection(context):
     if context.readDataFile('mars.r.txt') is None:
-        return 
+        return
     site = context.getSite()
     qi = getToolByName(site, 'portal_quickinstaller')
     i = 'plone.app.collection'
     if qi.isProductInstalled(i):
         qi.uninstallProducts([i])
 
-def setupCatalog(context):            
-    filename = context.readDataFile('marscatalog.xml')
-    if filename is None: return 
-    url = getToolByName(context, 'portal_url')
+def setupCatalog(context):
+    logger = logging.getLogger('mars.policy / setuphandler')
+    site = context.getSite()
+    xmlstr = context.readDataFile('marscatalog.xml')
+    if xmlstr is None: return
+    url = getToolByName(site, 'portal_url')
     site = url.getPortalObject()
     catalog = getToolByName(site, 'portal_catalog')
     added_indexes = []
-    added_columns = []
-
-
+    dom = etree.fromstring(xmlstr)
+    class Iextras(object): pass
+    for nindex in dom.xpath('//index'):
+        added = False
+        extras = Iextras()
+        infos = dict(nindex.items())
+        name = infos.get('name', '')
+        itype = infos.get('meta_type', '')
+        properties = {}
+        indexed_attr = name
+        iextras = nindex.xpath('extra')
+        iattrs = nindex.xpath('indexed_attr')
+        iproperties = nindex.xpath('property')
+        if iextras:
+            for p in iextras:
+                iname = dict(p.items())['name']
+                value = dict(p.items())['value']
+                setattr(extras, iname, value)
+        if iproperties:
+            for p in iproperties:
+                t = (p.xpath('text()')[0].strip().lower()
+                     == 'true')
+                iname = dict(p.items())['name']
+                if not iname in properties:
+                    properties[iname] = t
+        if iattrs:
+            lindexed_attr = []
+            for att in iattrs:
+                iname = dict(att.items())['value']
+                if not iname in lindexed_attr:
+                    lindexed_attr.append(iname)
+            if lindexed_attr:
+                indexed_attr = ','.join(lindexed_attr)
+        if itype == 'ZCTextIndex':
+            k = 'doc_attr'
+        else:
+            k = 'indexed_attr'
+        setattr(extras, k,  indexed_attr)
+        if name in catalog.Indexes: continue
+        if itype in ['KeywordIndex',
+                     'FieldIndex',
+                     'ZCTextIndex',
+                     'DateIndex',
+                    ]:
+            added = True
+        if added:
+            catalog.addIndex(name, itype, extra=extras)
+            if len(properties.keys()):
+                idx = catalog._catalog.getIndex(name)
+                for p in properties:
+                    idx._updateProperty(p, properties[p])
+            if not name in added_indexes:
+                added_indexes.append(name)
+    for nindex in dom.xpath('//column'):
+        name = dict(nindex.items()).get('value', '').strip()
+        if not name in catalog._catalog.schema.keys():
+            catalog.addColumn(name)
+    for c in added_indexes:
+        logger.info('Reindex %s' % c)
+        catalog.reindexIndex(c, site.REQUEST)
 
