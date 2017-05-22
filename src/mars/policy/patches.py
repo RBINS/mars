42935dcf622908d49c4aca5043e84ab7e72dbf5d
Thomas Desvenain <thomas.desvenain@gmail.com>
2017-05-22 22:24:01 +0200

    include patch allowing abbreviations on names starting with two bytes chars

---
+++ src/mars/policy/patches.py
@@ -0,0 +1,22 @@
+
+try:
+    from Products.ATExtensions.datatype import formattablename
+    HAS_ATEXTENSIONS = True
+except ImportError:
+    HAS_ATEXTENSIONS = False
+
+if HAS_ATEXTENSIONS:
+    if not hasattr(formattablename, '_old_abbreviate'):
+
+        formattablename._old_abbreviate = formattablename.abbreviate
+
+        def two_bytes_chars_capable_abbreviate(value, *args, **kwargs):
+            if type(value) == str:
+                value = unicode(value, encoding='utf-8')
+                abbrev = formattablename._old_abbreviate(value, *args, **kwargs)
+                return abbrev.encode('utf-8')
+            else:
+                return formattablename._old_abbreviate(value, *args, **kwargs)
+
+
+        formattablename.abbreviate = two_bytes_chars_capable_abbreviate
