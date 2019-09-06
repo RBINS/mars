efeaac14d4eba9ccf2e072ac20c22ebb614dfe3c
Thomas Desvenain <thomas.desvenain@gmail.com>
2019-09-06 15:38:14 +0200

    fix dexterity form

---
+++ src/mars/skin/skins/mars_skin_custom_templates/collectKeywords.py
@@ -0,0 +1,35 @@
+## Script (Python) "collectKeywords"
+##title=Edit content
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=name, index, vocab_source='portal_catalog'
+REQUEST = context.REQUEST
+
+#when using archetypes.schemaextender the keyword widget call to
+#field.getAccessor return None, so to let integrator do sth we
+#need to said index name is equal to field name.
+if index is None:
+    index = name
+
+## With the advent of multi-cataloging we need to pass an optional
+## catalog id to use for the widget.
+
+try:
+    allowed, enforce = context.Vocabulary(name)
+except AttributeError:
+    return ()
+
+catalog = getattr(container, vocab_source)
+previous = catalog.uniqueValuesFor(index)
+
+if enforce:
+    result = allowed
+else:
+    result = allowed + previous
+
+result = result.sortedByValue()
+
+return result
