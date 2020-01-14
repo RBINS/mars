ebc3d561863152e093428aba3f0fceea52d1a212
Gagaro <gagaro42@gmail.com>
2020-01-14 17:28:21 +0100

    fix #20152 : add index/metadata for relations

--- src/mars/policy/catalog.py
+++ src/mars/policy/catalog.py
@@ -4,10 +4,44 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from Products.CMFPlone.interfaces import IPloneSiteRoot
 from plone.indexer import indexer
 from plone.dexterity.interfaces import IDexterityItem
 
 from Products.Archetypes.interfaces.base import IBaseContent
+
+
+def filter_item(obj):
+    filtered = False
+    if IPloneSiteRoot.providedBy(obj):
+        filtered = True
+    if obj.getId() == 'organigram':
+        filtered = True
+    return filtered
+
+
+def relateditems_fullpathc(obj, titles=None):
+    if titles is None:
+        titles = list()
+    if not hasattr(obj, 'getId') or not hasattr(obj, 'Title'):
+        return ()
+    if not filter_item(obj):
+        titles.insert(0, obj.Title())
+        titles = relateditems_fullpathc(obj.aq_inner.aq_parent, titles)
+    return tuple(titles)
+
+
+@indexer(IBaseContent)
+def relateditems_fullpath(obj, titles=None):
+    items = []
+    values = []
+    try:
+        items = obj.getRelatedItems()
+    except Exception:
+        pass
+    for o in items:
+        values.extend(relateditems_fullpathc(o))
+    return ' '.join(values)
 
 
 def _Freedate(item):
